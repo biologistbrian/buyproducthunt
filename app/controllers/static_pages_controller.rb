29405a1a889d6df27489d6d33ece4a3248702673
biologistbrian <brianthebiologist@gmail.com>
2020-08-15 20:45:54 -0400

    testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
     req = HTTP.post('https://api.producthunt.com/v2/oauth/token', :json => { 
       "client_id": "zGwRsoqwyQsY2y9btTHA7mnWF0foKCxQID89LrLKeMY",
       "client_secret": "Lux6dn4exeK4LvhoD_0JK1X6ydfe74FpltcXdYdSCRo",
-      "redirect_uri": "https://www.dealflopro.com",
+      "redirect_uri": "https://buyproducthunt.herokuapp.com/booger",
       "code": code,
       "grant_type": "authorization_code"
       })
