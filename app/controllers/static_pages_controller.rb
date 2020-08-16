fce590041e58d250dc6199a62cbf8b10f1691664
biologistbrian <brianthebiologist@gmail.com>
2020-08-16 15:36:49 -0400

    hockey

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,7 +10,8 @@ class StaticPagesController < ApplicationController
       "code": code,
       "grant_type": "authorization_code"
       })
-    rit = HTTP.auth('Bearer '+code).post('https://api.producthunt.com/v2/api/graphql', :json => { 'query':'{viewer(){user{id, name, username,isMaker,twitterUsername,coverImage}}}'})
+    @b =  JSON.parse(req.to_s)
+    rit = HTTP.auth('Bearer '+@b['access_code']).post('https://api.producthunt.com/v2/api/graphql', :json => { 'query':'{viewer(){user{id, name, username,isMaker,twitterUsername,coverImage}}}'})
     @a = JSON.parse(rit.to_s)
   end
 end
